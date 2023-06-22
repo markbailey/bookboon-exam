6ca51114534ffd30476c3cf41758e53d1ef73a24
Mark Bailey <markbailey78@yahoo.co.uk>
2023-06-22 11:31:00 +0100

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,13 +8,14 @@ function Timer(props: TimerProps) {
   const width = (100 / startTime) * timeRemaining;
 
   return (
-    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-      <div
-        className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-        style={{ width: `${width}%` }}
-      >
-        {Math.round(timeRemaining)}
+    <div>
+      <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div
+          className="bg-blue-600 h-3 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+          style={{ width: `${width}%` }}
+        />
       </div>
+      <small>Time remaining: {Math.round(timeRemaining)} seconds</small>
     </div>
   );
 }
